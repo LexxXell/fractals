5806bf839acf03f65c42ea9b565d969627a7e158
LexxXell <lexxxell007@gmail.com>
2020-10-04 19:58:17 +0400

    Refactoring

--- L-system.py
+++ L-system.py
@@ -1,5 +1,12 @@
 import turtle
 
+turtle.hideturtle()
+turtle.tracer(0)
+turtle.penup()
+turtle.setposition(-380, 300)
+turtle.pensize(2)
+turtle.pendown()
+
 axiom = input("Enter the starting axiom (chars available: F+-):\n")
 itr = int(input("Enter the number of iterations:\n"))
 
@@ -17,15 +24,6 @@ while count <= itr:
         tmp_axiom += rule[char]
     axiom, tmp_axiom = tmp_axiom, ""
     count += 1
-
-print(axiom)
-
-turtle.hideturtle()
-turtle.tracer(0)
-turtle.penup()
-turtle.setposition(-380, 300)
-turtle.pensize(2)
-turtle.pendown()
 
 for char in axiom:
     if char == "+":
